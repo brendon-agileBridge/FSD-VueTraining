7f2e925264ae0d628b3bbecd36f0f3df94737564
Brendon VDM <brendonvdm@agilebridge.co.za>
2024-08-12 12:57:52 +0200

    Highlighting Active Page

--- vuetraining/src/App.vue
+++ vuetraining/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   Site Running
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/Totals">Totals</router-link>
+    <router-link class="navLink" active-class="active" to="/">Home</router-link> |
+    <router-link class="navLink" active-class="active" to="/Totals">Totals</router-link>
   </nav>
   
   
@@ -23,5 +23,20 @@
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+nav {
+  padding:1rem;
+  
+  .navLink{
+    text-decoration: none;
+    color: black;
+    padding: 0.3rem;
+  }
+  
+  .active {
+    background-color: #2c3e50;
+    color: white;
+    border-radius: 25px;
+  }
 }
 </style>

390c50d9940a81a830774ffaa030adb0f692bfc3
Brendon VDM <brendonvdm@agilebridge.co.za>
2024-08-12 12:29:27 +0200

    Computed Functions

--- vuetraining/src/components/CountComponent.vue
+++ vuetraining/src/components/CountComponent.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {ref, watch} from "vue";
+import {computed, ref, watch} from "vue";
 
 let props = defineProps({
   initialCount: Number
@@ -19,12 +19,27 @@ function resetCount() {
   count.value = 0;
 }
 
+const letter = computed(() => {
+  let ordA = 'a'.charCodeAt(0);
+  let ordZ = 'z'.charCodeAt(0);
+  let len = ordZ - ordA + 1;
+
+  let s = "";
+  let n = count.value;
+  while(n >= 0) {
+    s = String.fromCharCode(n % len + ordA) + s;
+    n = Math.floor(n / len) - 1;
+  }
+  return s;
+});
 
 </script>
 
 <template>
   <br><br><br>
   Count is: {{ count }}
+  <br>
+  Letter is: {{ letter }}
   <br>
   <label for="">Two way bound value</label>
   <br>

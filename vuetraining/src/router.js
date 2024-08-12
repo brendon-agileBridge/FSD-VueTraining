8a143f4f6365b68ce3810ab9510329905bf72f35
Brendon VDM <brendonvdm@agilebridge.co.za>
2024-08-12 12:49:26 +0200

    Creating a new Page

--- vuetraining/src/router.js
+++ vuetraining/src/router.js
@@ -1,9 +1,11 @@
 import {createWebHistory, createRouter} from 'vue-router'
 
 import IndexView from './pages/index.vue'
+import TotalsView from "@/pages/totals.vue";
 
 const routes = [
     { path: '/', component: IndexView },
+    { path: '/Totals', component: TotalsView },
 ]
 
 export default createRouter({

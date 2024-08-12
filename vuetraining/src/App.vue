24df874c8dea470a5ca48d39d62a713fe500e433
Brendon VDM <brendonvdm@agilebridge.co.za>
2024-08-12 12:03:05 +0200

    Creating a component

--- vuetraining/src/App.vue
+++ vuetraining/src/App.vue
@@ -1,21 +1,10 @@
 <template>
   Site Running
   <br>
-  Count is: {{ count }}
-  <br>
-  Alternatively Count is: <span v-text="count"></span>
-  <br><br><br>
-  <label for="">Two way bound value</label>
-  <br>
-  <input type="number" v-model="count">
-  <br>
-  <button @click="count = 0">Reset</button>
+  
 </template>
 
 <script setup>
-import {ref} from "vue";
-
-let count = ref(0);
 
 
 </script>
---
+++ vuetraining/src/components/CountComponent.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+import {ref} from "vue";
+
+let count = ref(0);
+
+</script>
+
+<template>
+  Count is: {{ count }}
+  <br>
+  Alternatively Count is: <span v-text="count"></span>
+  <br><br><br>
+  <label for="">Two way bound value</label>
+  <br>
+  <input type="number" v-model="count">
+  <br>
+  <button @click="count = 0">Reset</button>
+</template>
+
+<style scoped>
+
+</style>

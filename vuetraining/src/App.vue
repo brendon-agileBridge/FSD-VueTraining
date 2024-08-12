8aa25e34273a6025b60f443f2545450658980e00
Brendon VDM <brendonvdm@agilebridge.co.za>
2024-08-12 11:57:28 +0200

    2 Way Bound Data

--- vuetraining/src/App.vue
+++ vuetraining/src/App.vue
@@ -4,10 +4,16 @@
   Count is: {{ count }}
   <br>
   Alternatively Count is: <span v-text="count"></span>
+  <br><br><br>
+  <label for="">Two way bound value</label>
+  <br>
+  <input type="number" v-model="count">
 </template>
 
 <script setup>
-let count = 0;
+import {ref} from "vue";
+
+let count = ref(0);
 
 
 </script>
